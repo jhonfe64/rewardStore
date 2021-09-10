623b419c6b4382b5f943540006b36c7b4f30b646
John Fredy Pérez <jhonfe649@gmail.com>
2021-09-10 15:30:25 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
     <CoinsContextProvider>
     <JumboHeightContextProvider>
-    <Router basename="rewardStore">
+    <Router>
       <Switch>
         <Route path="/">
           <div className={`App ${successModalStatus && 'bodyBackground'}`} style={{marginBottom: '100px'}}>
